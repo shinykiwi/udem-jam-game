8c36f89ac7015ce9f6dbd9cc0071b2c9f4128a8c
NicolassBoyer <nicolasbernardboyer@gmail.com>
2025-05-04 13:35:18 -0400

    schoolbell sfx added

--- Assets/Scripts/UpgradeItem.cs
+++ Assets/Scripts/UpgradeItem.cs
@@ -12,7 +12,9 @@ public class UpgradeItem : ScriptableObject
 
     public bool isGood;
     public string upgradeName;
+    public string frenchName;
     public string description;
+    public string frenchDescription;
     public int cost;
 
     public float engagementGain;
