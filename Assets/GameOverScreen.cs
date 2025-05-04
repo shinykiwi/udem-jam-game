be7dd44f8003d3f00f4f251eef8e3d6df766b9d7
Kira <kafountain5@gmail.com>
2025-05-04 13:39:22 -0400

    pushing

---
+++ Assets/GameOverScreen.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameOverScreen : MonoBehaviour
+{
+    private Canvas canvas;
+    public static GameOverScreen Instance { get; private set; }
+    
+    private void Awake() 
+    { 
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+        }
+    }
+
+    private void Start()
+    {
+        canvas.enabled = false;
+    }
+
+    public void Show()
+    {
+        canvas.enabled = true;
+    }
+}
