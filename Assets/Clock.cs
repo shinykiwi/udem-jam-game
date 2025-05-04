572549cdfaf52273bca9d9b1ac1ab7e7318ee58b
Kira <kafountain5@gmail.com>
2025-05-04 13:06:55 -0400

    clock

---
+++ Assets/Clock.cs
@@ -0,0 +1,46 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    private float minutes = 0;
+    private float hours = 9;
+    
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    private void Update()
+    {
+        minutes += Time.deltaTime;
+
+        if (minutes > 59)
+        {
+            minutes = 0;
+            hours++;
+        }
+
+        string end = hours < 12 ? "AM" : "PM";
+        timeText.text = hours + ":" +minutes.ToString("00") + end;
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            SpeedUp();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SpeedNormal();
+        }
+        
+    }
+
+    private void SpeedUp()
+    {
+        Time.timeScale *= 3;
+    }
+
+    private void SpeedNormal()
+    {
+        Time.timeScale = 1;
+    }
+}
